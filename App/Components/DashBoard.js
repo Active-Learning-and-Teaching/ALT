09ba060cc6b6f11f3b21e02aebd86faa06334882
Dewangee <dewangee16034@iiitd.ac.in>
2020-04-23 04:03:42 +0530

    SignOut implemented on Dashboard.

--- App/Components/DashBoard.js
+++ App/Components/DashBoard.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import auth from '@react-native-firebase/auth'
 import {
+    Button,
     StyleSheet,
     Text,
     View,
+    Alert
 } from 'react-native';
 
 export default class DashBoard extends Component {
@@ -14,15 +16,28 @@ export default class DashBoard extends Component {
         };
     }
 
-    componentDidMount(){
-        const {username} = auth().currentUser.displayName
-        this.setState({username})
+    signOut = () => {
+        auth()
+            .signOut()
+            .then(
+                this.props.navigation.navigate('Login')
+            )
+            .catch(err => {
+                Alert.alert(err.message)
+            })
     }
+
+
+    // componentDidMount(){
+    //     // const {username} = auth().currentUser.displayName
+    //     // this.setState({username})
+    // }
 
     render(){
         return(
             <View style= {styles.container}>
-                <Text > WELCOME {this.state.username}</Text>
+                <Text > WELCOME </Text>
+                <Button style={styles.buttonMessage} title="SignOut" onPress={this.signOut} />
             </View>
         );
     }
@@ -41,8 +56,10 @@ const styles = StyleSheet.create({
     textStyle: {
         fontSize: 15,
         marginBottom: 20
+    },
+    buttonMessage: {
+        paddingTop : 10,
+        marginTop: 15
     }
-
-
 });
 
