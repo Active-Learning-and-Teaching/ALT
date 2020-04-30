9090b661842a54ee06f2b6aa3823f7bdc6ce572b
Dewangee <dewangee16034@iiitd.ac.in>
2020-04-30 10:51:17 +0530

    Faculy login implemented.

---
+++ App/Components/FacultyDashBoard.js
@@ -0,0 +1,73 @@
+import React, {Component} from 'react';
+import auth from '@react-native-firebase/auth'
+import {
+    Button,
+    StyleSheet,
+    Text,
+    View,
+    Alert
+} from 'react-native';
+import {GoogleSignin} from '@react-native-community/google-signin';
+
+export default class FacultyDashBoard extends Component {
+    constructor() {
+        super();
+        this.state = {
+            username : ''
+        };
+    }
+
+    signOut = async () => {
+        try {
+            await GoogleSignin.revokeAccess();
+            await GoogleSignin.signOut()
+            this.props.navigation.navigate('Login')
+        }
+        catch (error) {
+            auth()
+                .signOut()
+                .then(
+                    this.props.navigation.navigate('Login')
+                )
+                .catch(err => {
+                    Alert.alert(err.message)
+                })
+        }
+    }
+
+
+    // componentDidMount(){
+    //     // const {username} = auth().currentUser.displayName
+    //     // this.setState({username})
+    // }
+
+    render(){
+        return(
+            <View style= {styles.container}>
+                <Text > WELCOME Faculty</Text>
+                <Button style={styles.buttonMessage} title="SignOut" onPress={this.signOut} />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        display: "flex",
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 35,
+        backgroundColor: '#fff'
+    },
+    textStyle: {
+        fontSize: 15,
+        marginBottom: 20
+    },
+    buttonMessage: {
+        paddingTop : 10,
+        marginTop: 15
+    }
+});
+
---
+++ App/Databases/Faculty.js
@@ -0,0 +1,11 @@
+
+export default class Faculty {
+
+    id : number
+    name : string
+    email : string
+
+
+
+
+}
