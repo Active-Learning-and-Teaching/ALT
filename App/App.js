21577459f80e085fb867ac72af8c69e3b5356f8e
suraj16101 <suraj16101@iiitd.ac.in>
2020-07-11 00:24:22 +0530

    iOS Faculty Form UI, StudenFaculty Option Page, SwipeBack bug fixed for iOS

--- App/App.js
+++ App/App.js
@@ -12,6 +12,7 @@ import TabNavigator from './Components/TabNavigator';
 import IconF from 'react-native-vector-icons/FontAwesome';
 import IconM from 'react-native-vector-icons/MaterialCommunityIcons';
 import IconI from 'react-native-vector-icons/MaterialIcons';
+import StudentOrFaculty from './Components/StudentOrFaculty';
 IconF.loadFont();
 IconM.loadFont();
 IconI.loadFont();
@@ -32,26 +33,34 @@ function MyStack(){
                         options={{
                             headerTitle : "Loading",
                             headerLeft : null,
+                            gestureEnabled: false
                         }}/>
                 <Stack.Screen name = "Login" component={LogIn}
                           options={{
                               headerTitle : "Login",
                               headerLeft : null,
+                              gestureEnabled: false
                           }}/>
                 <Stack.Screen name = "Register User" component={RegisterUser}
                           options={{
                               headerTitle : "Register",
                           }}/>
+                <Stack.Screen name = "User Type" component={StudentOrFaculty}
+                              options={{
+                                  headerTitle : "Register",
+                              }}/>
                 <Stack.Screen name = "Student DashBoard" component={StudentDashBoard}
                           options={{
                               headerTitle : "Dashboard",
                               headerLeft : null,
+                              gestureEnabled: false
                               // headerRight : {CourseAdd}
                           }}/>
                 <Stack.Screen name = "Faculty DashBoard" component={FacultyDashBoard}
                               options={{
                                   headerTitle : "Dashboard",
                                   headerLeft : null,
+                                  gestureEnabled: false
                                   // headerRight : {CourseAdd}
                               }}/>
                 <Stack.Screen name = "Course" component={TabNavigator}
