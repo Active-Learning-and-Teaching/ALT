276f00997ea828a5f7f081ac94a88dbd6ddae493
Yashdeep Prasad <yashdeep18121@iiitd.ac.in>
2021-03-19 18:44:01 +0530

    NOTIFICATION through firestore

--- App/index.js
+++ App/index.js
@@ -5,5 +5,15 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import messaging from '@react-native-firebase/messaging';
+
+messaging()
+  .subscribeToTopic('Course')
+  .then(() => console.log('Subscribed to topic!'));
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log(remoteMessage);
+  console.log('Message handled in the background!', remoteMessage);
+});
 
 AppRegistry.registerComponent(appName, () => App);
