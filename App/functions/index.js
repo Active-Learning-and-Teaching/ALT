14ebe2bcbbac24a1f530b134d2229586b0cccb59
Divyam Gupta <divyamgupta@Divyams-MacBook-Pro.local>
2021-03-19 22:31:30 +0530

    Added optimised endpoint for faculty deletion

--- App/functions/index.js
+++ App/functions/index.js
@@ -46,6 +46,92 @@ deleteCourseHelper(passCode);
 res.send("Done deleting")
 });
 
+function removeFromStudentList(courseKey){
+  const student = admin.database().ref('InternalDb/Student/');
+  student.once("value", function(snapshot){
+    snapshot.forEach(el=>{
+      let studentKey = el.ref.path.pieces_.reverse()[0];
+      const thisStudent = admin.database().ref('InternalDb/Student/'+studentKey+'/courses');
+      thisStudent.once("value", function(snapshot){
+        snapshot.forEach((el)=>{
+          if(el.val() === courseKey){
+            el.ref.remove();
+          }
+        })
+      })
+    })
+  })
+}
+function removeCourseFromFacultyList(courseKey){
+  const student = admin.database().ref('InternalDb/Faculty/');
+  student.once("value", function(snapshot){
+    snapshot.forEach(el=>{
+      let facultyKey = el.ref.path.pieces_.reverse()[0];
+      const thisStudent = admin.database().ref('InternalDb/Faculty/'+facultyKey+'/courses');
+      thisStudent.once("value", function(snapshot){
+        snapshot.forEach((el)=>{
+          if(el.val() === courseKey){
+            el.ref.remove();
+          }
+        })
+      })
+    })
+  })
+}
+
+function removeFromFacultyList(key){
+  const faculty = admin.database().ref('InternalDb/Faculty/'+key+'/courses');
+  faculty.once("value", function(snapshot){
+    snapshot.forEach((el)=>{
+      removeFromStudentList(el.val())
+      el.ref.remove();
+    })
+  })
+}
+
+function delCoursesOfFaculty(facultyKey){
+  console.log("Called");
+  removeFromFacultyList(facultyKey);
+}
+
+exports.deleteFaculty = functions.https.onRequest((req,res) => {
+key = req.body['key'];
+console.log("Faculty KEY "+ key )
+db_ref = admin.database().ref('InternalDb/Faculty/'+key)
+db_ref.once("value", function(snapshot) 
+{
+  console.log(snapshot.val());
+  if(snapshot.val()['courses']){
+  snapshot.val()['courses'].forEach(function(child)
+  {
+    console.log("Removing course of key " + child);
+    course_ref = admin.database().ref('InternalDb/Courses/'+child)
+    course_ref.once("value", 
+      function(courseSnapshot){
+          var passcode  = courseSnapshot.val()['passCode'];
+          deleteCourseHelper(passcode);
+        }
+      ,
+      function (errorObject) {
+        console.log("The Course read failed: " + errorObject.code);
+        res.send("ERROR");
+      }
+    );
+  });
+  delCoursesOfFaculty(key);
+  snapshot.ref.remove();
+  res.send("removed");
+  }
+  else{
+    res.send("error while removing");
+  }
+}, function (errorObject) {
+  console.log("The faculty read failed: " + errorObject.code);
+  res.send("ERROR")
+});
+}
+);
+
 
 exports.sendNotificationToTopic_New = functions.firestore
   .document('Course/{uid}')
