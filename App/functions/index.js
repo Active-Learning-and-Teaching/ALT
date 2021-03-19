8ab727ca2a6798487ac73c5923d1723b2c9e7017
vishweshdkumar <vishwesh18119@iiitd.ac.in>
2021-03-19 22:25:19 +0530

    Added optimized endpoint for course deletion

--- App/functions/index.js
+++ App/functions/index.js
@@ -10,6 +10,42 @@ admin.initializeApp(functions.config().firebase);
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
+function deleteAllMatchingKey(table,key) {
+    const db_ref = admin.database().ref('InternalDb/'+table+'/');
+    db_ref.orderByChild("passCode").equalTo(key).once("value", function(snapshot) {
+      console.log('starting to remove from table '+table)
+      snapshot.forEach(function(child){
+        console.log(child.key);
+        child.ref.remove();
+      });
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+      res.send("ERROR")
+    });
+
+}
+function deleteCourseHelper(passCode){
+    deleteAllMatchingKey('Courses',passCode)
+    deleteAllMatchingKey('Announcements',passCode)
+    deleteAllMatchingKey('KBC',passCode)
+    deleteAllMatchingKey('KBCResponse',passCode)
+    deleteAllMatchingKey('Feedback',passCode)
+    deleteAllMatchingKey('FeedbackResponse',passCode)
+    removeFromStudentList(passCode)
+    removeCourseFromFacultyList(passCode)
+}
+
+exports.deleteCourse = functions.https.onRequest((req,res) => {
+
+//  flow : 1. del announcements
+// 2. del student registerations ,faculty registrations
+// getting all announcements and deleting them
+const passCode = req.body['passCode'];
+console.log("Got passCode to delete "+ passCode)
+deleteCourseHelper(passCode);
+res.send("Done deleting")
+});
+
 
 exports.sendNotificationToTopic_New = functions.firestore
   .document('Course/{uid}')
@@ -29,3 +65,5 @@ exports.sendNotificationToTopic_New = functions.firestore
     const response = await admin.messaging().send(message);
     console.log(response);
   });
+
+
