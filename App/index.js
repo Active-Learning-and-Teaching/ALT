d359765d835e100e6a56b5a67129735ad6afd95e
Yashdeep Prasad <yashdeep18121@iiitd.ac.in>
2021-04-07 20:50:14 +0530

    Removed topic subscription

--- App/index.js
+++ App/index.js
@@ -7,10 +7,6 @@ import App from './App';
 import {name as appName} from './app.json';
 import messaging from '@react-native-firebase/messaging';
 
-messaging()
-  .subscribeToTopic('Course')
-  .then(() => console.log('Subscribed to topic!'));
-
 messaging().setBackgroundMessageHandler(async remoteMessage => {
   console.log(remoteMessage);
   console.log('Message handled in the background!', remoteMessage);
