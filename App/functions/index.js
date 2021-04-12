ca10cc20660ce3443ea65d1208647c31bed7c302
Divyam Gupta <divyamgupta@Divyams-MacBook-Pro.local>
2021-04-12 18:13:32 +0530

    Fixing some testing issues

--- App/functions/index.js
+++ App/functions/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable spaced-comment */
 /* eslint-disable max-len */
 /* eslint-disable eol-last */
 /* eslint-disable no-var */
@@ -10,43 +9,61 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 // const url = 'https://testdb-cloudfn.firebaseio.com/'
 const url = 'https://testfortls.firebaseio.com/'
+// const url = 'https://tls-op-default-rtdb.firebaseio.com/'
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
-function deleteAllMatchingKey(table,key, childKey) {
-  const db_ref = admin.app().database(url).ref('InternalDb/'+table+'/');
-  db_ref.orderByChild(childKey).equalTo(key).once("value", function(snapshot) {
-    console.log('starting to remove from table '+table)
-    snapshot.forEach(function(child){
-      console.log(child.key);
-      child.ref.remove();
-    });
-  }, function (errorObject) {
-    console.log("The read failed: " + errorObject.code);
 
-  });
+async function getURLFromPasscode(passCode){
+    const db_ref = admin.app().database(url).ref('InternalDb/Courses/');
+    let courseURL;
+    await db_ref.orderByChild("passCode").equalTo(passCode).once("value",
+          function(snapshot) {
+            courseURL = Object.keys(snapshot.val())[0].replace(' ', '');
+          }, 
+          function (errorObject) {
+            console.log("The read failed: " + errorObject.code);
+          }
+    );
+    return courseURL
 }
 
-function deleteCourseHelper(passCode){
+function deleteAllMatchingKey(table,key, childKey) {
+    const db_ref = admin.app().database(url).ref('InternalDb/'+table+'/');
+    db_ref.orderByChild(childKey).equalTo(key).once("value", function(snapshot) {
+      console.log('starting to remove from table '+table)
+      snapshot.forEach(function(child){
+        console.log(child.key);
+        child.ref.remove();
+      });
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+
+    });
+
+}
+
+function deleteCourseHelper(passCode, courseURL){
     deleteAllMatchingKey('Courses',passCode, "passCode")
     deleteAllMatchingKey('Announcements',passCode, "passCode")
     deleteAllMatchingKey('KBC',passCode, "passCode")
     deleteAllMatchingKey('KBCResponse',passCode, "passCode")
     deleteAllMatchingKey('Feedback',passCode, "passCode")
     deleteAllMatchingKey('FeedbackResponse',passCode, "passCode")
-    removeFromStudentList(passCode)
-    removeCourseFromFacultyList(passCode)
+    removeFromStudentList(courseURL);
+    removeCourseFromFacultyList(courseURL);
 }
-exports.deleteCourse = functions.https.onCall((data,context) => {
 
-//  flow : 1. del announcements
-// 2. del student registerations ,faculty registrations
-// getting all announcements and deleting them
-// const passCode = req.body['passCode'];
-const passCode = data.passCode;
-console.log("Got passCode to delete "+ passCode)
-deleteCourseHelper(passCode);
-// res.send("Done deleting")
-return 'done';
+exports.deleteCourse = functions.https.onCall(async (data,context) => {
+
+  //  flow : 1. del announcements
+  // 2. del student registerations ,faculty registrations
+  // getting all announcements and deleting them
+  const passCode = data.passCode;
+  console.log("Got passCode to delete "+ passCode)
+  let courseURL = await getURLFromPasscode(passCode);
+  deleteCourseHelper(passCode, courseURL);
+  context.send("Done deleting")
+  return 'done';
 });
 
 function removeFromStudentList(courseKey){
@@ -70,7 +87,7 @@ function removeCourseFromFacultyList(courseKey){
   student.once("value", function(snapshot){
     snapshot.forEach(el=>{
       let facultyKey = el.ref.path.pieces_.reverse()[0];
-      const thisStudent = admin.app().database().ref('InternalDb/Faculty/'+facultyKey+'/courses');
+      const thisStudent = admin.app().database(url).ref('InternalDb/Faculty/'+facultyKey+'/courses');
       thisStudent.once("value", function(snapshot){
         snapshot.forEach((el)=>{
           if(el.val() === courseKey){
@@ -135,63 +152,49 @@ exports.deleteStudent = functions.https.onCall((data, context) =>{
 });
 
 exports.deleteFaculty = functions.https.onCall((data,context) => {
-// key = req.body['key'];
-key = data.key
-uid = data.uid
-console.log("Faculty KEY "+ key )
-console.log("recieved data")
-console.log(data)
-console.log(context)
-db_ref = admin.app().database(url).ref('InternalDb/Faculty/'+key)
-db_ref.once("value", function(snapshot) 
-{
-  console.log(snapshot.val());
-  if(snapshot.val()['courses']){
-  snapshot.val()['courses'].forEach(function(child)
+  // key = data.body['key'];
+  key = data.key;
+  console.log("Faculty KEY "+ key )
+  console.log("recieved data")
+  console.log(data)
+  console.log(context)
+  db_ref = admin.app().database(url).ref('InternalDb/Faculty/'+key)
+  db_ref.once("value", function(snapshot) 
   {
-    console.log("Removing course of key " + child);
-    course_ref = admin.app().database(url).ref('InternalDb/Courses/'+child)
-    course_ref.once("value", 
-      function(courseSnapshot){
-          var passcode  = courseSnapshot.val()['passCode'];
-          deleteCourseHelper(passcode);
+    console.log(snapshot.val());
+    if(snapshot.val()['courses']){
+    snapshot.val()['courses'].forEach(function(child)
+    {
+      console.log("Removing course of key " + child);
+      course_ref = admin.app().database(url).ref('InternalDb/Courses/'+child)
+      course_ref.once("value", 
+        function(courseSnapshot){
+            var passcode  = courseSnapshot.val()['passCode'];
+            deleteCourseHelper(passcode, child);
+          }
+        ,
+        function (errorObject) {
+          console.log("The Course read failed: " + errorObject.code);
+          // res.send("ERROR");
+          return "Error";
         }
-      ,
-      function (errorObject) {
-        console.log("The Course read failed: " + errorObject.code);
-        // res.send("ERROR");
-        return "Error";
-      }
-    );
+      );
+    });
+    delCoursesOfFaculty(key);
+    snapshot.ref.remove();
+    // res.send("removed");
+    return "removed"
+    }
+    else{
+      // res.send("error while removing");
+      return "error while removing"
+    }
+  }, function (errorObject) {
+    console.log("The faculty read failed: " + errorObject.code);
+    // res.send("ERROR")
+    return "Error";
   });
-  delCoursesOfFaculty(key);
-  snapshot.ref.remove();
-  // res.send("removed");
-  return "removed"
-  }
-  else{
-    // res.send("error while removing");
-    return "error while removing"
-  }
-}, function (errorObject) {
-  console.log("The faculty read failed: " + errorObject.code);
-  // res.send("ERROR")
-  return "Error";
 });
-   
-admin
-  .auth()
-  .deleteUser(uid)
-  .then(() => {
-    console.log('Successfully deleted user from firebase auth');
-  })
-  .catch((error) => {
-    console.log('Error deleting user from firebase auth:', error);
-  });
-
-
-}
-);
 
 exports.sendNotificationToTopic_New = functions.firestore
   .document('Course/{uid}')
@@ -207,32 +210,9 @@ exports.sendNotificationToTopic_New = functions.firestore
       topic: 'Course',
     };
 
+    console.log(message);
     const response = await admin.messaging().send(message);
     console.log(response);
   });
 
-
-exports.sendPushNotification = functions.database
-  .ref('InternalDb/Student/{sid}') // Put your path here with the params.
-  .onWrite(async (change, context) => {
-    try {
-      const {after} = change;
-      const {_data} = after;
-      console.log(_data);
-      //const {deviceToken} = await firebase.messaging().getToken();
-      //if (!deviceToken) return;
-
-      const payload = {
-        notification: {
-          title: 'Notification',
-          body: `FCM notification triggered!`,
-        },
-        topic: 'Course', // Passing the path params along with the notification to the device. [optional]
-      };
-
-      return await admin.messaging().send(payload);
-    } catch (ex) {
-      return console.error('Error:', ex.toString());
-    }
-  });
 
