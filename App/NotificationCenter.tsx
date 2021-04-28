d54f0bc06e402e2087566c8d5bb6bbb5d023d741
Yashdeep Prasad <yashdeep18121@iiitd.ac.in>
2021-04-29 00:53:37 +0530

    Permission changes for IOS

--- App/NotificationCenter.tsx
+++ App/NotificationCenter.tsx
@@ -87,7 +87,7 @@ const NotifiactionCentre: FC = () => {
 
   useEffect(() => {
     const unsubscribe = messaging().onMessage(remoteMessage => {
-      console.log(remoteMessage.notification);
+      console.log(remoteMessage);
       if (remoteMessage.notification) {
         transition.setValue(300);
         const enterAnim = Animated.spring(transition, {
@@ -122,7 +122,7 @@ const NotifiactionCentre: FC = () => {
     <>
       <Animated.View
         style={[
-          tailwind('absolute top-0 z-10 inset-x-0 px-2 mb-2'),
+          tailwind('absolute bottom-0 z-10 inset-x-0 px-2 mb-2'),
           {transform: [{translateY: transition}, {translateX: swipe}], opacity},
         ]}>
         <TouchableOpacity
