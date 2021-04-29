4e6280aa2defd422260e1555461c645385c7bedb
Yashdeep Prasad <45161513+HackGod2000@users.noreply.github.com>
2021-04-30 01:29:02 +0530

    Fix background iOS

--- App/index.js
+++ App/index.js
@@ -8,56 +8,39 @@ import {name as appName} from './app.json';
 import messaging from '@react-native-firebase/messaging';
 import {typography} from './Utils/typography';
 
-
 typography();
 
 async function requestUserPermission() {
   const authStatus = await messaging().requestPermission();
-  const enabled =
-    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-
-  if (enabled) {
-    console.log('Authorization status:', authStatus);
-  }
+  console.log(authStatus);
 }
 
-messaging()
-  .hasPermission()
-  .then(enabled => {
-    if (enabled) {
-      messaging()
-        .getToken()
-        .then(token => {
-          console.log('getting Token');
-          console.log(token);
-        })
-        .catch(error => {
-          /* handle error */
-          console.log(error);
-        });
-    } else {
-      messaging()
-        .requestPermission()
-        .then(() => {
-          console.log('Requesting Permission');
-          /* got permission */
-        })
-        .catch(error => {
-          /* handle error */
-          console.log(error);
-        });
-    }
-  })
-  .catch(error => {
-    /* handle error */
-    console.log(error);
-  });
+requestUserPermission();
 
-
+// messaging()
+//   .hasPermission()
+//   .then(enabled => {
+//     if (enabled) {
+//       messaging()
+//         .getToken()
+//         .then(token => {
+//           console.log('getting Token');
+//           console.log(token);
+//         })
+//         .catch(error => {
+//           /* handle error */
+//           console.log(error);
+//         });
+//     }
+//   })
+//   .catch(error => {
+//     /* handle error */
+//     console.log(error);
+//   });
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {
-  console.log(remoteMessage);
+  console.log('----------');
+  console.log('In Back Ground');
   console.log('Message handled in the background!', remoteMessage);
 });
 
