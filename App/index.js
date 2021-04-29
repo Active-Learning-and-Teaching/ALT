61c0383ed4b44fe66ab027d71ef7249d84042888
Yashdeep Prasad <yashdeep18121@iiitd.ac.in>
2021-04-30 03:22:50 +0530

    xklmx

--- App/index.js
+++ App/index.js
@@ -6,12 +6,41 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import messaging from '@react-native-firebase/messaging';
-import { typography } from './Utils/typography'
+import {typography} from './Utils/typography';
 
-typography()
+typography();
+
+async function requestUserPermission() {
+  const authStatus = await messaging().requestPermission();
+  console.log(authStatus);
+}
+
+requestUserPermission();
+
+// messaging()
+//   .hasPermission()
+//   .then(enabled => {
+//     if (enabled) {
+//       messaging()
+//         .getToken()
+//         .then(token => {
+//           console.log('getting Token');
+//           console.log(token);
+//         })
+//         .catch(error => {
+//           /* handle error */
+//           console.log(error);
+//         });
+//     }
+//   })
+//   .catch(error => {
+//     /* handle error */
+//     console.log(error);
+//   });
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {
-  console.log(remoteMessage);
+  console.log('----------');
+  console.log('In Back Ground');
   console.log('Message handled in the background!', remoteMessage);
 });
 
