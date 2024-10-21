d749d047c7b59cb087420c3786635b943f8f0cfa
FakePickle <harsh22200@iiitd.ac.in>
2024-10-21 08:54:01 +0530

    chore: completing login signup page and setting up firebase backend

--- app-example/(tabs)/index.tsx
+++ app-example/(tabs)/index.tsx
@@ -8,7 +8,7 @@ import { ThemedView } from '@/components/ThemedView';
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: '#A020F0', dark: '#1D3D47' }}
       headerImage={
         <Image
           source={require('@/assets/images/partial-react-logo.png')}
